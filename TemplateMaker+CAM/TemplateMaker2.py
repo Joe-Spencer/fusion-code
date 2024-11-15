c81f5cee861ca38caea7ab1c93c9b2954023e0de
Joe <31321326+Joe-Spencer@users.noreply.github.com>
2024-11-15 13:24:32 -0500

    Optimized toolpathing in TemplateMaker + CAM

--- TemplateMaker+CAM/TemplateMaker2.py
+++ TemplateMaker+CAM/TemplateMaker2.py
@@ -1,5 +1,4 @@
 import adsk.core, adsk.fusion, adsk.cam, traceback
-import math
 import os
 from enum import Enum
 
@@ -10,12 +9,12 @@ from enum import Enum
 MILLING_TOOL_LIBRARY = 'Milling Tools (Metric)'
 
 # Some material properties for feed and speed calculation
-ALUMINUM_CUTTING_SPEED = 300  # mm/min
-ALUMINUM_FEED_PER_TOOTH = 0.1 # mm/tooth
+WOOD_CUTTING_SPEED = 300  # mm/min
+WOOD_FEED_PER_TOOTH = 0.1 # mm/tooth
 
 # some tool preset name (which we know exists for the selected tools)
-ALUMINUM_PRESET_ROUGHING = 'alu* rou*'
-ALUMINUM_PRESET_FINISHING = 'Aluminum - Finishing'
+WOOD_PRESET_ROUGHING = 'alu* rou*'
+WOOD_PRESET_FINISHING = 'WOOD - Finishing'
 
 
 #################### Some useful enumerators ####################
@@ -24,7 +23,6 @@ class ToolType(Enum):
     BULL_NOSE_END_MILL = 'bull nose end mill'
     BALL_END_MILL = 'ball end mill'
     FACE_MILL = 'face mill'
-
 
 # Setup work coordinate system (WCS) location (enumerator)
 class SetupWCSPoint(Enum):
@@ -166,78 +164,17 @@ def run(context):
         # set setup origin
         setup.parameters.itemByName('wcs_origin_boxPoint').value.value = SetupWCSPoint.BOTTOM_XMAX_YMAX.value
 
-
-        #################### face operations ####################
-        # calculate feed and speed for face operation
-        toolDiameter = faceTool.parameters.itemByName('tool_diameter').value.value          # cm
-        numberOfFlutes = faceTool.parameters.itemByName('tool_numberOfFlutes').value.value  # int
-        spindleSpeed = ALUMINUM_CUTTING_SPEED / math.pi / (toolDiameter * 10) * 1000        # rpm
-        cuttingFeedrate = spindleSpeed * ALUMINUM_FEED_PER_TOOTH * numberOfFlutes           # mm/min
-
-        # create a preset with those calculated feeds
-        facePreset = faceTool.presets.add()
-        facePreset.name = 'Aluminum (set by script)'
-        facePreset.parameters.itemByName('tool_spindleSpeed').value.value = int(spindleSpeed)
-        facePreset.parameters.itemByName('tool_feedCutting').expression = str(int(cuttingFeedrate)) + ' mm/min'
-
-        # create a face operation input
-        input = setup.operations.createInput('face')
-        input.tool = faceTool
-        input.toolPreset = facePreset # assign created preset
-        input.displayName = 'Face Operation'       
-        input.parameters.itemByName('tolerance').expression = '0.01 mm'
-        input.parameters.itemByName('stepover').expression = '0.75 * tool_diameter'
-        input.parameters.itemByName('direction').expression = "'climb'"
-
-        # determine pass angle along largest part dimension
-        # get stock box dimensions in cm
-        stockX = setup.parameters.itemByName('job_stockInfoDimensionX').value.value
-        stockY = setup.parameters.itemByName('job_stockInfoDimensionY').value.value
-        # determine pass angle to be along largest length (X or Y) of the block
-        if stockX >= stockY:
-            input.parameters.itemByName('passAngle').expression = '0 deg' 
-        else:
-            input.parameters.itemByName('passAngle').expression = '90 deg'
-
-        # add the operation to the setup
-        faceOp = setup.operations.add(input)
-
-
-        #################### scribe operation ####################
-        scribe_sketch = None
-        for sketch in design.rootComponent.sketches:
-            if sketch.name == "Scribe":
-                ui.messageBox("Scribe sketch found")
-                scribe_sketch = sketch
-                break
-        input=setup.operations.createInput('trace')
-        geometry = []
-        for profile in scribe_sketch.profiles:
-            for curve in profile.profileLoops[0].profileCurves:
-                geometry.append(curve.sketchEntity)
-
-        modelParam = input.parameters.itemByName('model')
-
-
-        traceOP = setup.operations.add(input)
-        sketchSelection: adsk.cam.CadContours2dParameterValue = traceOP.parameters.itemByName('sketches').value
-        scribe = sketchSelection.getCurveSelections()
-        scribe = sketchSelection.createNewChainSelection()
-        scribe.inputGeometry = [scribe_sketch]
-        sketchSelection.applyCurveSelections(scribe)
-
-        # geomSelect: adsk.cam.SketchSelection = modelParam.value
-        # geomSelect.value = geometry
-        # input.parameters.itemByName('geometry').expression = geometry
-        # input.tool = adaptiveTool
-        # input.geometry = geometry
-        
-        input.displayName = 'Scribe'
+        #################### adaptive operations ####################
+        input = setup.operations.createInput('adaptive')
+        input.tool = adaptiveTool
+        input.displayName = 'Adaptive Roughing'
         input.parameters.itemByName('tolerance').expression = '0.1 mm' 
+        input.parameters.itemByName('maximumStepdown').expression = '5 mm' 
+        input.parameters.itemByName('fineStepdown').expression = '0.25 * maximumStepdown'
+        input.parameters.itemByName('flatAreaMachining').expression = 'false'
 
-               
-        # look if there is a tool preset related to aluminum roughing
-        presets = adaptiveTool.presets.itemsByName(ALUMINUM_PRESET_ROUGHING)
+        # look if there is a tool preset related to WOOD roughing
+        presets = adaptiveTool.presets.itemsByName(WOOD_PRESET_ROUGHING)
         if len(presets) > 0:
             # we pick and use the first preset found
             adaptivePreset = presets[0]
@@ -246,20 +183,39 @@ def run(context):
         # add the operation to the setup
         adaptiveOp = setup.operations.add(input)
 
+        #################### scribe operation ####################
+        scribe_sketch = None
+        for sketch in design.rootComponent.sketches:
+            if sketch.name == "Scribe":
+                ui.messageBox("Scribe sketch found")
+                scribe_sketch = sketch
+                break
+        
+
+
+        geometry = []
+        for profile in scribe_sketch.profiles:
+            for curve in profile.profileLoops[0].profileCurves:
+                geometry.append(curve.sketchEntity)
+        traceOP = setup.operations.add(input)
+        # contourParam: adsk.cam.CadContours2dParameterValue = traceOP.parameters.itemByName('contours').value
+        # curveSelections = contourParam.getCurveSelections()
+        # chain = curveSelections.createNewChainSelection()
+        input=setup.operations.createInput('trace')
 
         #################### finishing tool preset ####################
         # get a tool preset from the finishing tool
         finishingPreset = None
-        presets = finishingTool.presets.itemsByName(ALUMINUM_PRESET_FINISHING)
+        presets = finishingTool.presets.itemsByName(WOOD_PRESET_FINISHING)
         if len(presets) > 0:
-            # use the first aluminum finishing preset found
+            # use the first WOOD finishing preset found
             finishingPreset = presets[0]
 
 
-        #################### parallel operations ####################
+        #################### cutting operations ####################
         input = setup.operations.createInput('parallel')
         input.tool = finishingTool
-        input.displayName = 'Parallel Finishing'
+        input.displayName = 'Parallel Cutting'
         input.parameters.itemByName('tolerance').expression = '0.01 mm'
         input.parameters.itemByName('cuspHeightStepover').expression = '0.005 mm'
         input.parameters.itemByName('boundaryMode').expression = "'selection'"
@@ -287,48 +243,10 @@ def run(context):
             cadcontours2dParam.applyCurveSelections(chains)
 
 
-        #################### steep and shallow operations ####################
-        # Create folder for finishing operations that require Machining Extension
-        operationInput = setup.operations.createInput('folder')
-        operationInput.displayName = 'Machining Extension Required'
-        folder: adsk.cam.CAMFolder = setup.operations.add(operationInput)
-
-        # Create steep and shallow operation in the folder
-        input = setup.operations.createInput('steep_and_shallow')
-        input.tool = finishingTool
-        input.displayName = 'Steep and Shallow Finishing'
-        input.parameters.itemByName('tolerance').expression = '0.01 mm'
-        input.parameters.itemByName('useAvoidFlats').expression = 'true'
-        input.parameters.itemByName('cuspHeightStepdown').expression = '0.005 mm'
-        input.parameters.itemByName('cuspHeightStepover').expression = 'cuspHeightStepdown'
-        input.parameters.itemByName('spiral').expression = 'true'
-        input.parameters.itemByName('shallowSpiral').expression = 'true'
-        input.parameters.itemByName('offsetSmoothing').expression = 'true'
-        if finishingPreset:
-            # assign the finishig tool preset
-            input.toolPreset = finishingPreset
-
-        # add the operation to the folder
-        steepAndShallowOp = folder.operations.add(input)
-
-        # check if this toolpath is generatable ("steep_and_shallow" required the manufacturing extension)
-        isSteepAndShallowGeneratable = False
-        for op in setup.operations.compatibleStrategies:
-            if op.name == 'steep_and_shallow':
-                if op.isGenerationAllowed:
-                    # isGenerationAllowed will be false if the extension isn't active which prevent from generating the steep_and_shallow operation
-                    isSteepAndShallowGeneratable = True
-                break
-
-
         #################### generate operations ####################
         # list the valid operations to generate
         operations = adsk.core.ObjectCollection.create()
-        operations.add(faceOp)
-        operations.add(adaptiveOp)
         operations.add(parallelOp)
-        if isSteepAndShallowGeneratable:
-            operations.add(steepAndShallowOp)
 
         # create progress bar
         progressDialog = ui.createProgressDialog()
@@ -365,9 +283,11 @@ def run(context):
 
         # find the "XYZ" post in the post library and import it to local library
         for config in postConfigs:
-            if config.description == 'Custom Thermwood 3-Axis':
+            ui.messageBox("config found"+config.description)
+            if config.description == 'Thermwood':
                 url = adsk.core.URL.create("user://")
-                importedURL = postLibrary.importPostConfiguration(config, url, "CustomThermwood - v3.cps")
+                ui.messageBox("pp found")
+                importedURL = postLibrary.importPostConfiguration(config, url, "Thermwood")
 
         # get the imported local post config
         postConfig = postLibrary.postConfigurationAtURL(importedURL)
@@ -386,8 +306,8 @@ def run(context):
         desktopDirectory = os.path.expanduser("~/Desktop").replace('\\', '/') 
         ncParameters.itemByName('nc_program_output_folder').value.value = desktopDirectory
         
-        # select the operations to generate (we skip steep_and_shallow here)
-        ncInput.operations = [faceOp, adaptiveOp, parallelOp]
+        # select the operations to generate
+        ncInput.operations = [adaptiveOp, parallelOp]
 
         # add a new ncprogram from the ncprogram input
         newProgram = cam.ncPrograms.add(ncInput)
